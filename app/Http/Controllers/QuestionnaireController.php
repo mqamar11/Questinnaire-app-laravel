033116d0bae88801f95554f8cdfa097b8eeffc7a
mqamar11 <qamarmaqsood11@gmail.com>
2021-07-01 13:08:41 -0700

    initial commit

---
+++ app/Http/Controllers/QuestionnaireController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use illuminate\Http\Request;
+
+
+class QuestionnaireController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function create()
+    {
+        return view('questionnaire.create');
+    }
+
+    public function store()
+    {
+        $data = request()->validate([
+            'title'   => 'required',
+            'purpose' => 'required',
+        ]);
+
+         $data ['user_id'] = auth()->user()->id;
+         $questionnaire = \App\Questionnaire::create($data);
+
+        // $questionnaire = auth()->user()->questionnaires()->create($data);
+
+           return redirect('/questionnaires/'.$questionnaire->id);
+    }
+
+    public function show(\App\Questionnaire $questionnaire)
+    {
+        $questionnaire->load('questions.answers.responses');
+
+        return view('questionnaire.show', compact('questionnaire'));
+    }
+
+}
